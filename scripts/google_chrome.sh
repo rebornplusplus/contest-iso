c4b7df788da10ef21e65d7fb1aa98e13621c5879
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2022-09-11 02:53:57 +0600

    Will remove Google Chrome deb after installing

--- scripts/google_chrome.sh
+++ scripts/google_chrome.sh
@@ -1,3 +1,5 @@
 echo "Installing Google Chrome.."
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb
+rm -v google-chrome-stable_current_amd64.deb
