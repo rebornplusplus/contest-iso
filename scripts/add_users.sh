a917a8c0226097bea53d7e3e38d8100938d20608
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2022-09-11 05:44:30 +0600

    Updated shell in add_users

--- scripts/add_users.sh
+++ scripts/add_users.sh
@@ -1,8 +1,8 @@
 source vars.sh
 
 echo "Creating admin account, $ADMIN_USER..."
-useradd --create-home --password $(echo $ADMIN_PASSWORD) $ADMIN_USER
+useradd --create-home --password $(echo $ADMIN_PASSWORD) --shell /bin/bash $ADMIN_USER
 usermod -aG sudo $ADMIN_USER
 
 echo "Creating team account, $TEAM_USER..."
-useradd --create-home --password $(echo $TEAM_PASSWORD) $TEAM_USER
+useradd --create-home --password $(echo $TEAM_PASSWORD) --shell /bin/bash $TEAM_USER
