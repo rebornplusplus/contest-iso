3375f4d76dc8661568e44a653a58aac9a2be469f
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2023-01-25 03:12:54 +0600

    Copy the dot files to /etc/skel
    Files from /etc/skel are copied to each time a new user
    is created, to their home directory. Keep the dot files
    in /etc/skel to make them available for any new user.

--- scripts/copy_dots.sh
+++ scripts/copy_dots.sh
@@ -2,13 +2,8 @@ source vars.sh
 
 cd $DOTS_DIR
 
-for user in $(echo "$ADMIN_USER $TEAM_USER $MOCK_USER"); do
-	homedir="/home/$user"
-	for file in .[^.]*; do
-		cp -rv $file "$homedir/"
-		chown -R -c -v $user:$user "$homedir/$file"
-		chmod -R -v 750 "$homedir/$file"
-	done
+for file in .[^.]*; do
+	cp -rv $file "/etc/skel"
 done
 
 cd -
