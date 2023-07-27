6da0268b407246fe93abcfa29f1e50671a4330e4
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2023-07-27 21:12:48 +0600

    Updated pycharm_community.sh

--- scripts/pycharm_community.sh
+++ scripts/pycharm_community.sh
@@ -4,15 +4,23 @@ set -eux
 
 PYCHARM_VERSION="2023.1.4"
 
-echo "Install Pycharm Community $PYCHARM_VERSION .."
+echo "\nInstall Pycharm Community $PYCHARM_VERSION ..\n"
 
 # download pycharm community edition
 # wget "data.services.jetbrains.com/products/download?code=PCC&platform=linux" -O pycharm.tar.gz
 wget -c "https://download.jetbrains.com/python/pycharm-community-${PYCHARM_VERSION}.tar.gz"
 
+# download sha256sum file and verify the checksum
+wget "https://download.jetbrains.com/python/pycharm-community-${PYCHARM_VERSION}.tar.gz.sha256" -O expected_sha256sum.txt
+sha256sum pycharm-community-${PYCHARM_VERSION}.tar.gz >> actual_sha256sum.txt
+cat actual_sha256sum.txt | sha256sum -c expected_sha256sum.txt
+
 # extract and keep the files in /opt
 tar -zxvf "pycharm-community-${PYCHARM_VERSION}.tar.gz"
 mv "pycharm-community-${PYCHARM_VERSION}" /opt
+
+# remove tar and hash files
+rm "pycharm-community-${PYCHARM_VERSION}.tar.gz" actual_sha256sum.txt expected_sha256sum.txt
 
 # populate desktop entry
 cat > jetbrains-pycharm.desktop << EOF
