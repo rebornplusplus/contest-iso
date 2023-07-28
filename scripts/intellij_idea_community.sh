525f412c212680b8c65161168483ae1b83632c58
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2023-07-28 22:19:59 +0600

    Parameterized idea-ce by version no

--- scripts/intellij_idea_community.sh
+++ scripts/intellij_idea_community.sh
@@ -2,22 +2,24 @@
 
 set -eux
 
-echo "\nInstalling IntelliJ IDEA Community Edition 2023.2 ...\n"
+IDEA_VERSION='2023.2'
+echo "\nInstalling IntelliJ IDEA Community Edition $IDEA_VERSION ...\n"
 
 # download pycharm community edition
-wget -c "https://download.jetbrains.com/idea/ideaIC-2023.2.tar.gz"
+wget -c "https://download.jetbrains.com/idea/ideaIC-$IDEA_VERSION.tar.gz"
 
 # download sha256sum file and verify the checksum
-wget "https://download.jetbrains.com/idea/ideaIC-2023.2.tar.gz.sha256" -O expected_sha256sum.txt
-sha256sum ideaIC-2023.2.tar.gz >> actual_sha256sum.txt
+wget "https://download.jetbrains.com/idea/ideaIC-$IDEA_VERSION.tar.gz.sha256" -O expected_sha256sum.txt
+sha256sum ideaIC-$IDEA_VERSION.tar.gz >> actual_sha256sum.txt
 cat actual_sha256sum.txt | sha256sum -c expected_sha256sum.txt
 
 # extract and keep the files in /opt
-tar -zxvf "ideaIC-2023.2.tar.gz"
-mv "idea-IC-232.8660.185" /opt
+mkdir idea-ce-$IDEA_VERSION
+tar -zxvf "ideaIC-$IDEA_VERSION.tar.gz" -C idea-ce-$IDEA_VERSION --strip-components 1
+mv idea-ce-$IDEA_VERSION /opt
 
 # remove tar and hash files
-rm "ideaIC-2023.2.tar.gz" actual_sha256sum.txt expected_sha256sum.txt
+rm "ideaIC-$IDEA_VERSION.tar.gz" actual_sha256sum.txt expected_sha256sum.txt
 
 # populate desktop entry
 cat > jetbrains-idea-ce.desktop << EOF
@@ -25,8 +27,8 @@ cat > jetbrains-idea-ce.desktop << EOF
 Version=1.0
 Type=Application
 Name=IntelliJ IDEA Community Edition
-Icon=/opt/idea-IC-232.8660.185/bin/idea.svg
-Exec="/opt/idea-IC-232.8660.185/bin/idea.sh" %f
+Icon=/opt/idea-ce-$IDEA_VERSION/bin/idea.svg
+Exec="/opt/idea-ce-$IDEA_VERSION/bin/idea.sh" %f
 Comment=Capable and Ergonomic IDE for JVM
 Categories=Development;IDE;
 Terminal=false
