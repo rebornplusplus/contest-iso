1e2cfce14dab759ffc75edfbd802716ed0d6674c
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2024-02-18 02:17:16 +0600

    feat: install python3.11 instead of 3.10

--- scripts/python3.10.sh
+++
@@ -1,5 +0,0 @@
-echo "Installing Python 3.10..."
-
-apt install -y software-properties-common
-add-apt-repository -y ppa:deadsnakes/ppa
-apt install -y python3.10-full
---
+++ scripts/python3.11.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eux
+
+echo "Installing Python 3.11..."
+
+apt install -y python3.11-full
