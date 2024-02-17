08fee02c1c58067e1d40414d79492db3e8959506
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2024-02-18 02:26:15 +0600

    feat: add script to remove unnecessary packages

---
+++ scripts/trim.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script trims down the base Ubuntu 22.04 rootfs.
+# It removes unnecessary (for our purposes) packages like
+# thunderbird, libreoffice, rhythmbox, aisleriot etc.
+
+set -eux
+
+apt remove -y --purge \
+	aisleriot \
+	gnome-mahjongg \
+	gnome-mines \
+	gnome-sudoku \
+	'libreoffice*' \
+	rhythmbox \
+	shotwell \
+	thunderbird \
+	zsys
+
+# remove unused packages
+apt autoremove -y
