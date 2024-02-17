b12ae6681e1fe1d9abd8dcd0efd2311946949b7b
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2024-02-18 02:56:08 +0600

    feat: support for comments and empty lines in run_order file
    This commit adds support for comments and empty lines in the
    run_order.txt file. Comments must start with '#' character, without the
    quotes. The following is now supported:
    
        # === Install packages ===
    
        # Browsers
        google_chrome.sh
    
        # Compilers
        gcc-12.sh
        python3.11.sh
        openjdk17.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,14 @@ export DEBCONF_NONINTERACTIVE_SEEN=true
 
 shdir="scripts"
 
-for bashfile in $(cat $shdir/run_order.txt); do
-	echo -e "\nExecuting $bashfile ...\n"
-	bash "$shdir/$bashfile"
+run_order="$shdir/run_order.txt"
+if [[ ! -f "$run_order" ]]; then
+	echo "error: run_order.txt not found" >&2
+	exit 1
+fi
+
+scripts=( $(cat "$run_order" | grep "\S" | grep -v "^#" ) )
+for script in "${scripts[@]}"; do
+	echo -e "\nExecuting $script ...\n"
+	bash "$shdir/$script"
 done
