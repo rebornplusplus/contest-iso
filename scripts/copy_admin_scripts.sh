8083894e8f92d864bc32cdd42b540beed9c1d9a1
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2024-10-21 22:48:54 +0600

    fix wrong variable in copy_admin_scripts

--- scripts/copy_admin_scripts.sh
+++ scripts/copy_admin_scripts.sh
@@ -9,7 +9,7 @@ cd $ADMIN_SCRIPTS_DIR
 homedir="/home/$ADMIN_USER"
 for file in *; do
 	cp -rv $file "$homedir/"
-	chown -R -c -v $user:$user "$homedir/$file"
+	chown -R -c -v $ADMIN_USER:$ADMIN_USER "$homedir/$file"
 	chmod -R -v 750 "$homedir/$file"
 done
 
