62d9c7778833d4436619dcc3d469641788ed9fc3
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2025-11-28 03:43:35 -0500

    fix(scripts/chrome): install dependencies on fail

--- scripts/google_chrome.sh
+++ scripts/google_chrome.sh
@@ -5,5 +5,5 @@ set -eux
 echo "Installing Google Chrome.."
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb || apt-get install -f -y
 rm -v google-chrome-stable_current_amd64.deb
